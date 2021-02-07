20278e8670f1f4fabfc647b695ce6124c7a04fca
Yuri <Yuribtr@users.noreply.github.com>
2021-02-08 00:32:33 +0300

    Completed tasks: 1,2,3

---
+++ dynamic-templates/task1/app/templatetags/custom_filters.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def get_value(dictionary, key):
+    return dictionary[key]
+
+
+@register.filter
+def make_float(value):
+    try:
+        result = float(value)
+    except ValueError:
+        result = value
+    return result
+
+
+# @register.filter
+# def get_length(array):
+#     return len(array)
