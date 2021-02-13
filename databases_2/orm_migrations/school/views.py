06e05b46bde8a1956e9497f541ce189d8ca9480a
Yuri <Yuribtr@users.noreply.github.com>
2021-02-13 16:31:28 +0300

    Made "orm_migrations" homework

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -1,15 +1,10 @@
 from django.views.generic import ListView
 from django.shortcuts import render
-
-from .models import Student
+from school.models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
-
+    students = [*Student.objects.order_by('group').prefetch_related('teachers')]
+    context = {'object_list': students}
     return render(request, template, context)
--- databases_2/orm_migrations/website/settings.py
+++ databases_2/orm_migrations/website/settings.py
@@ -73,9 +73,11 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_orm_migrations',
+        'NAME': 'test4',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'test',
+        'PASSWORD': 'test',
     }
 }
 
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Moscow'
 
@@ -131,3 +133,23 @@ try:
 except ImportError:
     pass
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+
+INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+INTERNAL_IPS = ['127.0.0.1']
