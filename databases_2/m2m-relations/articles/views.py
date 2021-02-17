b283f892c4c51cb709318396fdeb45dac0f014b0
Yuri <Yuribtr@users.noreply.github.com>
2021-02-17 18:55:18 +0300

    Fixed detection of article's main scope

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,10 +1,10 @@
 from django.views.generic import ListView
 from django.shortcuts import render
-from articles.models import Article
+from articles.models import Article, ArticleScope
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    articles = [*Article.objects.order_by('-published_at').prefetch_related('scopes')]
+    articles = [*Article.objects.order_by('-published_at').prefetch_related('scopes__articles', 'scopes')]
     context = {'object_list': articles}
     return render(request, template, context)
