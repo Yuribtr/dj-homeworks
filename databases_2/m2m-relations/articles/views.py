542db7ecf80a9df7da47a3df56125778d1a65b74
Yuri <Yuribtr@users.noreply.github.com>
2021-02-17 23:41:28 +0300

    Minor fixing of HTML and view

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -5,13 +5,13 @@ from articles.models import Article, ArticleScope
 
 def articles_list(request):
     template = 'articles/news.html'
-    articles = [*Article.objects.order_by('-published_at').prefetch_related(
+    articles = Article.objects.order_by('-published_at').prefetch_related(
         Prefetch(
             'scopes',
             queryset=ArticleScope.objects.select_related(
                 'scope'
             ).order_by('-is_main'),
         ),
-    )]
+    )
     context = {'object_list': articles}
     return render(request, template, context)
